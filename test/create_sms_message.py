4bad26acae7b01505275ea4e43bb171ed860273e
Joe McBride <jmcbride@rackspace.com>
2015-06-03 11:02:49 -0500

    Update create_sms_message.py

--- test/create_sms_message.py
+++ test/create_sms_message.py
@@ -3,8 +3,8 @@ from twilio.rest import TwilioRestClient
 from datetime import datetime
  
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "AC616fbbf5c3dedbedf36db0bbb38853f5"
-auth_token  = "aa3c91e49e8c8995a1be057f96cd33e5"
+account_sid = ""
+auth_token  = ""
 current_time = str(datetime.now())
 message = "SweetBoardTest: " + current_time
 client = TwilioRestClient(account_sid, auth_token)
